8da0314c3bd0506a87aaff3fcbc7bbfe1fa29fce
Chris Adams <cadams@getaroom.com>
2022-05-12 22:17:21 +0100

    added basic form template example

--- app/models/users.py
+++ app/models/users.py
@@ -1,10 +1,11 @@
-from . import db
+from .. import db
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    created = db.Column(db.DateTime())
 
     def __repr__(self):
         return f'<User {self.name} ({self.id})>'

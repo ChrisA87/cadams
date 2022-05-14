ee941ce3197033c37543697160d8cbf585913570
Chris Adams <cadams@getaroom.com>
2022-05-14 11:44:48 +0100

    updated conftest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,4 @@
 import pytest
-from app import create_app
-
-
-@pytest.fixture
-def app():
-    app = create_app('test')
-    app.config["TESTING"] = True
-    yield app
-
-
-@pytest.fixture
-def client(app):
-    yield app.test_client()
 
 
 def test_get_index_returns_200(client):

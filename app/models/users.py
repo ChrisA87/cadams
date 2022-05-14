3ae9d84491ac05065df1d43994f98dd692eabc49
Chris Adams <cadams@getaroom.com>
2022-05-14 11:10:55 +0100

    updated models

--- app/models/users.py
+++ app/models/users.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     created = db.Column(db.DateTime())
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'<User {self.name} ({self.id})>'

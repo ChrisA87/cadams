f60ca80f355f878e57f9764f407007b480bdeb1c
Chris Adams <cadams@getaroom.com>
2022-05-22 18:45:56 +0100

    added stock_prices table definition

--- app/models/stocks.py
+++ app/models/stocks.py
@@ -18,13 +18,29 @@ class Stock(db.Model):
     name = db.Column(db.String(64), unique=True)
 
     @staticmethod
-    def insert_roles():
-        for id, (symbol, name) in enumerate(starting_stocks, 1):
+    def insert_stocks(stocks=starting_stocks):
+        for symbol, name in stocks:
             stock = Stock.query.filter_by(symbol=symbol).first()
             if stock is None:
-                stock = Stock(symbol=symbol, name=name, id=id)
+                stock = Stock(symbol=symbol, name=name)
                 db.session.add(stock)
                 db.session.commit()
 
     def __repr__(self):
-        return f"<Stock {self.symbol} - {self.name} ({self.id})>"
+        return f"<Stock [{self.id}]: {self.name} ({self.symbol})>"
+
+
+class StockPrice(db.Model):
+    __tablename__ = 'stock_prices'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime())
+    symbol = db.Column(db.String(10), db.ForeignKey('stocks.symbol'), index=True)
+    open = db.Column(db.Float)
+    close = db.Column(db.Float)
+    adj_close = db.Column(db.Float)
+    high = db.Column(db.Float)
+    low = db.Column(db.Float)
+    volume = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<StockPrice[{self.id}]: {self.symbol} - {self.date}>'

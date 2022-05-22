3bac514e27732ec306839a5b920ead2995e56074
Chris Adams <cadams@getaroom.com>
2022-05-22 18:46:50 +0100

    updated run_app shell context

--- run_app.py
+++ run_app.py
@@ -2,6 +2,7 @@ import os
 from app import create_app, db
 from app.models.users import User
 from app.models.roles import Role
+from app.models.stocks import Stock, StockPrices
 from flask_migrate import Migrate
 
 config_name = os.environ.get('FLASK_ENV') or 'default'
@@ -11,4 +12,4 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Stock=Stock, StockPrices=StockPrices)

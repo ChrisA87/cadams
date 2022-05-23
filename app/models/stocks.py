541927e6400caccf1e6028ba2e5dd94e09e04647
Chris Adams <cadams@getaroom.com>
2022-05-23 18:58:51 +0100

    added helped query to insert starting data

--- app/models/stocks.py
+++ app/models/stocks.py
@@ -1,4 +1,9 @@
+from datetime import datetime, timedelta
+import pandas_datareader as pdr
+import pandas as pd
+
 from .. import db
+
 
 starting_stocks = [
     ('FB', 'Facebook, Inc.'),
@@ -44,3 +49,24 @@ class StockPrice(db.Model):
 
     def __repr__(self):
         return f'<StockPrice[{self.id}]: {self.symbol} - {self.date.date()}>'
+
+    @staticmethod
+    def _insert_stock_prices():
+        date_ref = datetime(2022, 5, 22)
+        start = date_ref - timedelta(days=365*10)
+        symbols = [x for x, _ in starting_stocks]
+
+        df = pdr.yahoo.daily.YahooDailyReader(symbols=symbols, start=start).read()
+
+        if StockPrice.query.first() is None:
+            for sym in symbols:
+                tmp = (df.xs(sym, axis=1, level=1)
+                        .dropna().reset_index()
+                        .assign(symbol=sym)
+                        .rename(columns=lambda x: x.replace(' ', '_').lower()))
+
+                stock_prices = [StockPrice(**d) for d in tmp.to_dict(orient='records')]
+                db.session.add_all(stock_prices)
+                db.session.commit()
+        else:
+            print('stock_prices table is not empty')

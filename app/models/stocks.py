4d0ac05f5a21f46eab9eab7006827798aac77887
Chris Adams <cfh1990@msn.com>
2022-05-23 18:19:19 +0100

    Add stocks table (#16)
    * reset migrations
    
    * added stock table
    
    * tidied notebooks
    
    * fixed basedir
    
    * added stock_prices table definition
    
    * updated example notebook
    
    * updated requirements
    
    * updated run_app shell context
    
    * updated migrations
    
    * corrected names
    
    * updated tests

---
+++ app/models/stocks.py
@@ -0,0 +1,46 @@
+from .. import db
+
+starting_stocks = [
+    ('FB', 'Facebook, Inc.'),
+    ('AAPL', 'Apple, Inc.'),
+    ('SONY', 'Sony Group Corporation'),
+    ('MSFT', 'Microsoft Corporation'),
+    ('TTWO', 'Take-Two Interactive Software, Inc.'),
+    ('NFLX', 'Netflix, Inc.'),
+    ('SNOW', 'Snowflake, Inc.')
+]
+
+
+class Stock(db.Model):
+    __tablename__ = 'stocks'
+    id = db.Column(db.Integer, primary_key=True)
+    symbol = db.Column(db.String(64), unique=True)
+    name = db.Column(db.String(64), unique=True)
+
+    @staticmethod
+    def insert_stocks(stocks=starting_stocks):
+        for symbol, name in stocks:
+            stock = Stock.query.filter_by(symbol=symbol).first()
+            if stock is None:
+                stock = Stock(symbol=symbol, name=name)
+                db.session.add(stock)
+                db.session.commit()
+
+    def __repr__(self):
+        return f"<Stock[{self.id}]: {self.name} ({self.symbol})>"
+
+
+class StockPrice(db.Model):
+    __tablename__ = 'stock_prices'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime())
+    symbol = db.Column(db.String(10), db.ForeignKey('stocks.symbol'), index=True)
+    open = db.Column(db.Float)
+    close = db.Column(db.Float)
+    adj_close = db.Column(db.Float)
+    high = db.Column(db.Float)
+    low = db.Column(db.Float)
+    volume = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<StockPrice[{self.id}]: {self.symbol} - {self.date.date()}>'

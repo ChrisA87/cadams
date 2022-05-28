5bceea2ef945f39b048d12ee1f3603d38ac3dd5e
Chris Adams <cadams@getaroom.com>
2022-05-28 10:56:54 +0100

    updates tests

--- tests/test_main_views.py
+++ tests/test_main_views.py
@@ -3,3 +3,15 @@ def test_get_index_returns_200(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"Check back soon" in response.data
+
+
+def test_get_stocks_returns_200(client):
+    response = client.get('/stocks')
+    assert response.status_code == 200
+    assert b"Available Stocks" in response.data
+
+
+def test_get_stocks_plot_returns_200(client):
+    response = client.get('/stocks/test')
+    assert response.status_code == 200
+    assert b"Trading Strategy For TEST" in response.data

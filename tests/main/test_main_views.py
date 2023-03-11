76133fcd72bed401ecf8318e7a5e2e8685a5cbb3
Chris Adams <cfh1990@msn.com>
2023-03-11 17:29:32 +0000

    updated tests in main and auth views

--- tests/main/test_main_views.py
+++ tests/main/test_main_views.py
@@ -2,4 +2,4 @@
 def test_get_index_returns_200(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Check back soon" in response.data
+    assert b"Trading Ideas" in response.data

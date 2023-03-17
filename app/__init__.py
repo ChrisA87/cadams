516cab8feacfbc5ed9cd7317521b5cb4858f96da
Chris Adams <cfh1990@msn.com>
2023-03-17 22:33:18 +0000

    Adds CustomerAdminIndexView - updates visibility

--- app/__init__.py
+++ app/__init__.py
@@ -25,10 +25,10 @@ def create_app(config_name='default'):
     toolbar.init_app(app)
 
     # Set up admin page
-    admin = Admin(template_mode='bootstrap3', url='/auth/admin')
-    admin.init_app(app)
+    from .auth.admin import AdminModelView, CustomAdminIndexView
     from .models import users, stocks
-    from .auth.admin import AdminModelView
+    admin = Admin(template_mode='bootstrap3', index_view=CustomAdminIndexView(url='/auth/admin'))
+    admin.init_app(app)
     admin.add_view(AdminModelView(users.User, db.session))
     admin.add_view(AdminModelView(stocks.Stock, db.session))
     admin.add_view(AdminModelView(stocks.StockPrice, db.session))
--- app/auth/admin.py
+++ app/auth/admin.py
@@ -1,6 +1,15 @@
 from flask import redirect, url_for
 from flask_login import current_user
 from flask_admin.contrib.sqla import ModelView
+from flask_admin import AdminIndexView
+
+
+class CustomAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.role_id == 3
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth.login'))
 
 
 class AdminModelView(ModelView):

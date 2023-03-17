731383867e1f264eadf0ec99ffa5135231b256e4
Chris Adams <cadams@getaroom.com>
2023-03-17 09:10:49 +0000

    updates db setup - inserts roles

--- app/db/setup.py
+++ app/db/setup.py
@@ -1,12 +1,15 @@
 import os
 from app import create_app
 from app.models.stocks import Stock, StockPrice
+from app.models.roles import Role
 
 
 def main():
     app = create_app(os.environ.get('FLASK_ENV', 'default'))
     with app.app_context():
-        print('Inserting starter stocks...', end=' ')
+        print('Inserting roles...', end=' ')
+        Role.insert_roles()
+        print('Done\nInserting starter stocks...', end=' ')
         Stock.insert_stocks()
         print('Done\nInserting stock prices...')
         StockPrice.update()

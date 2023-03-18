ce52292806181e11450007f9dae2ad8bafcbb1b0
Chris Adams <cfh1990@msn.com>
2023-03-18 21:16:48 +0000

    adds test about page

--- tests/main/test_main_views.py
+++ tests/main/test_main_views.py
@@ -3,3 +3,9 @@ def test_get_index_returns_200(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"Trading Ideas" in response.data
+
+
+def test_get_about_returns_200(client):
+    response = client.get('/about-me')
+    assert response.status_code == 200
+    assert b"About" in response.data

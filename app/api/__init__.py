6423da4e1ca845743555d2358b5af84e52473fc8
Chris Adams <cfh1990@msn.com>
2023-03-18 14:09:36 +0000

    adds api bluprint

---
+++ app/api/__init__.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from flask_restx import Api
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+
+blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+api = Api(
+    app=blueprint,
+    title='Cadams APIs',
+    description=('Use the public key "974004f8-c594-11ed-976c-86ad6bb37ab4" '
+                 'to access the public endpoints.'),
+    authorizations=authorizations,
+    security='apikey')
+
+from .trading import api as namespace_trading
+
+api.add_namespace(namespace_trading)

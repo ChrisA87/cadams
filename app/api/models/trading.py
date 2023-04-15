ea617fc9b2515928632cd62a93a11300407b8d6b
Chris Adams <cfh1990@msn.com>
2023-04-15 12:34:08 +0100

    adds strategy param models

--- app/api/models/trading.py
+++ app/api/models/trading.py
@@ -10,3 +10,24 @@ sma_params = api.model('ParamsSMA', {
     'slow': fields.Integer(
         example=252,
         description='Slow moving average')})
+
+
+momentum_params = api.model('ParamsMomentum', {
+    'period': fields.Integer(
+        example=3,
+        description='The number of rolling days to use as a signal.')})
+
+
+mean_reversion_params = api.model('ParamsMeanReversion', {
+    'sma': fields.Integer(
+        example=28,
+        description='Simple moving average.'),
+    'threshold': fields.Float(
+        example=3.0,
+        description='Divergence from the mean threshold for signal.')})
+
+
+ols_params = api.model('ParamsOLS', {
+    'lags': fields.Integer(
+        example=7,
+        description='The number of lagged days to use for the signal.')})
